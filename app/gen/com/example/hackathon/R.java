957f4fc7e402f637052ce97ed4e8e36dec792961
SeeuD <Maxi-z350@gmx.de>
2014-10-31 21:28:16 +0100

    ham weitergemacht lena is nun am werk

--- app/gen/com/example/hackathon/R.java
+++ app/gen/com/example/hackathon/R.java
@@ -37,7 +37,9 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int card_layout=0x7f050004;
         public static final int hello_world=0x7f050001;
+        public static final int rectangle_card=0x7f050005;
         public static final int search_title=0x7f050003;
     }
     public static final class style {
