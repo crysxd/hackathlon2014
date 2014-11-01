cdd7e1ceca9a6c819082fd99ace9a493b40f47f5
Uaargh <ann-sophie.dietrich@hs-furtwangen.de>
2014-11-01 17:11:36 +0100

    Filling informations in bubbles

--- web/js/index.js
+++ web/js/index.js
@@ -1,7 +1,9 @@
 var personData = undefined;
+var currentName = undefined;
 
 $(function() {
-   
+  $('.bubble').hide();
+
     
 });
 
@@ -13,12 +15,54 @@ $('#btn-search').click(function() {
 function loadName(name) {
   $('#btn-search').button('loading');
 
+  $('.bubble').fadeOut(400); 
+  currentName = name;
+
   loadUrl('http://192.168.1.93/data.php?name=' + name, function(json) {
     personData = json;
-    alert(json.personCount);
+
+    $('.deg315 h4').html('Kürzeste Lebensdauer');
+    $('.deg315 p').html(json.min_life_span + ' Jahre');
+    $('.deg0 h4').html('Längste Lebensdauer');
+    $('.deg0 p').html(json.max_life_span + ' Jahre');
+    $('.deg45 h4').html('Durschnittliche Lebensdauer');
+    $('.deg45 p').html(Math.round(json.life_span) + ' Jahre');
+    $('.deg90 h4').html('Erste Erscheinung');
+    $('.deg90 p').html('Jahr ' + json.oldest_entry);
     
+    $('.deg135 h4').html('Häufigster Beruf');
+    if(json.occupations.length) {
+      $('.deg135 p').html(json.occupations[0].occupation);
+      
+    } else {
+      $('.deg135 p').html('--');
+
+    }
+    
+    $('.deg180 h4').html('Häufigster Geburtsort');
+    if(json.place_of_birth.length) {
+      $('.deg180 p').html(json.place_of_birth[0].place_of_birth);
+      
+    } else {
+      $('.deg180 p').html('--');
+
+    }
+    
+    $('.deg225 h4').html('Häufigster Lebensort');
+      if(json.place_of_activity.length) {
+      $('.deg225 p').html(json.place_of_activity[0].place_of_activity);
+      
+    } else {
+      $('.deg225 p').html('--');
+
+    }
+    
+    $('.deg270 h4').html('Anzahl Personen');
+    $('.deg270 p').html(json.personCount);
+
     $('#btn-search').button('reset');
-    
+    $('.bubble').fadeIn(400);
+
   });
 }
 
