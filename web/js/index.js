d61507771a2fcfc6d0fa55d1491196fbebd45900
Uaargh <ann-sophie.dietrich@hs-furtwangen.de>
2014-11-02 09:05:53 +0100

    Added prson tab and other improvements

--- web/js/index.js
+++ web/js/index.js
@@ -3,67 +3,154 @@ var currentName = undefined;
 
 $(function() {
   $('.bubble').hide();
-
+  $('#toolbar').hide();
+  selectTab($('#tab-stats'), true);
     
 });
+
+function selectTab(elem, noAnimation) {
+  var left = elem.position().left;
+  var width = elem.outerWidth();
+  
+  if(noAnimation) {
+    $('#tab-slider').css('left', left);
+    $('#tab-slider').css('width', width);
+    return;
+    
+  }
+  
+  $('#tab-slider').animate({left: left+'px', width: width+'px'}, 100);
+  
+}
 
 $('#btn-search').click(function() {
   loadName($('#search-field').val());
 
 });
 
-function loadName(name) {
-  $('#btn-search').button('loading');
+$('#tab-stats').click(function() {
+  showStats();
+  
+});
 
-  $('.bubble').fadeOut(400); 
-  currentName = name;
+$('#tab-persons').click(function() {
+  showPersons();
 
-  loadUrl('http://192.168.1.93/data.php?name=' + name, function(json) {
-    personData = json;
+});
 
-    $('.deg315 h4').html('Kürzeste Lebensdauer');
-    $('.deg315 p').html(json.min_life_span + ' Jahre');
-    $('.deg0 h4').html('Längste Lebensdauer');
-    $('.deg0 p').html(json.max_life_span + ' Jahre');
-    $('.deg45 h4').html('Durschnittliche Lebensdauer');
-    $('.deg45 p').html(Math.round(json.life_span) + ' Jahre');
+function showStats() {
+  selectTab($('#tab-stats'));
+
+  fadeBubblesOut(function() {
+
+    $('.deg315 p').html(personData.min_life_span + ' Jahre');
+    $('.deg315 h4').html('Kürzeste Lebenszeit');
+    $('.deg0 p').html(personData.max_life_span + ' Jahre');
+    $('.deg0 h4').html('Längste Lebenszeit');
+    $('.deg45 p').html(Math.round(personData.life_span) + ' Jahre');
+    $('.deg45 h4').html('Durchschnittliche Lebenszeit');
+    $('.deg90 p').html('Jahr ' + personData.oldest_entry);
     $('.deg90 h4').html('Erste Erscheinung');
-    $('.deg90 p').html('Jahr ' + json.oldest_entry);
-    
+    $('.deg270 p').html(personData.personCount);
+    $('.deg270 h4').html('Anzahl Personen');
     $('.deg135 h4').html('Häufigster Beruf');
-    if(json.occupations.length) {
-      $('.deg135 p').html(json.occupations[0].occupation);
-      
+    $('.deg180 h4').html('Häufigster Geburtsort');
+    $('.deg225 h4').html('Häufigster Lebensort');
+
+    if(personData.occupations.length) {
+      $('.deg135 p').html(personData.occupations[0].occupation);
+
     } else {
       $('.deg135 p').html('--');
 
     }
-    
-    $('.deg180 h4').html('Häufigster Geburtsort');
-    if(json.place_of_birth.length) {
-      $('.deg180 p').html(json.place_of_birth[0].place_of_birth);
-      
+
+    if(personData.place_of_birth.length) {
+      $('.deg180 p').html(personData.place_of_birth[0].place_of_birth);
+
     } else {
       $('.deg180 p').html('--');
 
     }
-    
-    $('.deg225 h4').html('Häufigster Lebensort');
-      if(json.place_of_activity.length) {
-      $('.deg225 p').html(json.place_of_activity[0].place_of_activity);
-      
+
+    if(personData.place_of_activity.length) {
+      $('.deg225 p').html(personData.place_of_activity[0].place_of_activity);
+
     } else {
       $('.deg225 p').html('--');
 
     }
     
-    $('.deg270 h4').html('Anzahl Personen');
-    $('.deg270 p').html(json.personCount);
-
-    $('#btn-search').button('reset');
-    $('.bubble').fadeIn(400);
-
+    fadeBubblesIn();
+    
   });
+}
+
+function showPersons() {
+  selectTab($('#tab-persons'));
+
+  fadeBubblesOut(function() {
+
+    $('.deg315 p').html('Name hier her');
+    $('.deg315 h4').html('VIP 1');
+    $('.deg0 p').html('Name hier her');
+    $('.deg0 h4').html('VIP 2');
+    $('.deg45 p').html('Name hier her');
+    $('.deg45 h4').html('VIP 3');
+    $('.deg90 p').html('Name hier her');
+    $('.deg90 h4').html('VIP 4');
+    $('.deg270 p').html('Name hier her');
+    $('.deg270 h4').html('VIP 5');
+    $('.deg135 p').html('Name hier her');
+    $('.deg135 h4').html('VIP 6');
+    $('.deg180 p').html('Name hier her');
+    $('.deg180 h4').html('VIP 7');
+    $('.deg225 p').html('Name hier her');
+    $('.deg225 h4').html('VIP 8');
+    
+    fadeBubblesIn();
+    
+  });
+}
+
+function fadeBubblesOut(callback) {
+  var bubbleCount = $('.bubble').length;
+    $('.bubble').fadeOut(300, function() {
+     bubbleCount--;
+     
+     if(bubbleCount == 0 && typeof callback === 'function')
+       callback();
+     
+   });
+  
+  /*$('.bubble').each(function(i, el) {
+    setTimeout(function() {
+        el.fadeOut(300);                           
+
+    },  100*i);
+  });*/
+}
+
+function fadeBubblesIn() {
+   $('.bubble').fadeIn(300);
+}
+
+function loadName(name) {
+  $('#btn-search').button('loading');
+  currentName = name;
+
+  fadeBubblesOut(function() {
+    loadUrl('http://192.168.1.60/data.php?name=' + name, function(json) {
+      personData = json;
+
+      $('#btn-search').button('reset');
+      $('#toolbar').fadeIn(300);
+      selectTab($('#tab-stats'), true);
+
+      showStats();
+
+    });
+  }); 
 }
 
 function loadUrl(url, callback) {
